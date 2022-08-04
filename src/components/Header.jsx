8db8d580948042a644432c3d8b4db3023e5d22c3
Abdelhk <72513211+abdoulbaguimohamadou@users.noreply.github.com>
2022-08-04 16:23:49 +0100

    firebase and auth with google config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,31 @@
 import styled from "styled-components"
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { auth, provider } from "../firebase"
 
 const Header = (props) => {
+    const handleAuth = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential =
+                    GoogleAuthProvider.credentialFromResult(result)
+                const token = credential.accessToken
+                // The signed-in user info.
+                const user = result.user
+                // ...
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code
+                const errorMessage = error.message
+                // The email of the user's account used.
+                const email = error.customData.email
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error)
+                // ...
+            })
+    }
+
     return (
         <Navigation>
             <Logo>
@@ -32,7 +57,7 @@ const Header = (props) => {
                     <span>Series</span>
                 </a>
             </Menu>
-            <Login>Login</Login>
+            <Login onClick={handleAuth}>Login</Login>
         </Navigation>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore/lite"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getStorage } from "firebase/storage"
+// import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUh_eX8y5NJkgEUgFCbvnaytgaEmGaqf8",
+    authDomain: "disney-plus-e9f0a.firebaseapp.com",
+    projectId: "disney-plus-e9f0a",
+    storageBucket: "disney-plus-e9f0a.appspot.com",
+    messagingSenderId: "514743193399",
+    appId: "1:514743193399:web:864cdeb5ff2add50e114b2",
+    measurementId: "G-71V9R8KXW5",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+auth.languageCode = getAuth().useDeviceLanguage()
+const provider = new GoogleAuthProvider()
+const storage = getStorage(app)
+// const analytics = getAnalytics(app)
+
+export { auth, provider, storage }
+export default db
