6522460aa23f1efcbeb154710b4b67cc3330a50d
Abdelhk <72513211+abdoulbaguimohamadou@users.noreply.github.com>
2022-08-11 23:49:07 +0100

    wrap images on viewer in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components"
 import ImgSlider from "./ImgSlider"
+import Viewers from "./Viewers"
 
 const Home = () => {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     )
 }
