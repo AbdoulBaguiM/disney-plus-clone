a3a6a18fa5f5540fe46f4600c105fcf43ec914ff
Abdelhk <72513211+abdoulbaguimohamadou@users.noreply.github.com>
2022-08-11 23:33:38 +0100

    images slider config finished

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -15,15 +15,29 @@ const ImgSlider = (props) => {
 
     return (
         <Carousel {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
+            <Wrap>
+                <a>
+                    <img src="/images/slider-badging.jpg" alt="" />
+                </a>
+            </Wrap>
+
+            <Wrap>
+                <a>
+                    <img src="/images/slider-scale.jpg" alt="" />
+                </a>
+            </Wrap>
+
+            <Wrap>
+                <a>
+                    <img src="/images/slider-badag.jpg" alt="" />
+                </a>
+            </Wrap>
+
+            <Wrap>
+                <a>
+                    <img src="/images/slider-scales.jpg" alt="" />
+                </a>
+            </Wrap>
         </Carousel>
     )
 }
@@ -55,9 +69,36 @@ const Carousel = styled(Slider)`
         overflow: initial;
     }
     .slick-prev {
-        left: -75px;
+        left: -65px;
     }
     .slick-next {
-        right: -75px;
+        right: -65px;
+    }
+`
+
+const Wrap = styled.div`
+    border-radius: 4px;
+    cursor: pointer;
+    position: relative;
+
+    a {
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 69%) 0 26px 30px --10px,
+            rgb(0 0 0 / 73%) 0 16px 10px -10px;
+        cursor: pointer;
+        display: block;
+        position: relative;
+        padding: 4px;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+
+    &:hover {
+        padding: 0;
+        border: 4px solid rgba(249, 249, 249, 0.8);
+        transition-duration: 300ms;
     }
 `
