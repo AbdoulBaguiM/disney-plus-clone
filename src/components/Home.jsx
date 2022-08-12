ced49ddc97b86f0d259093e5d0f1ecaec07370d0
Abdelhk <72513211+abdoulbaguimohamadou@users.noreply.github.com>
2022-08-12 22:03:15 +0100

    home update with movies

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,66 @@ import Originals from "./Originals"
 import Recommends from "./Recommends"
 import Trends from "./Trends"
 import Viewers from "./Viewers"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import db from "../firebase"
+import { setMovies } from "../features/movie/movieSlice"
+import { selectUserName } from "../features/user/userSlice"
 
-const Home = () => {
+const Home = (props) => {
+    const dispatch = useDispatch()
+    const userName = useSelector(selectUserName)
+
+    let recommends = []
+    let originals = []
+    let newDisney = []
+    let trends = []
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                console.log(recommends)
+                switch (doc.data().type) {
+                    case "recommend":
+                        recommends = [
+                            ...recommends,
+                            { id: doc.id, ...doc.data() },
+                        ]
+                        break
+
+                    case "new":
+                        newDisney = [
+                            ...newDisney,
+                            { id: doc.id, ...doc.data() },
+                        ]
+                        break
+
+                    case "original":
+                        originals = [
+                            ...originals,
+                            { id: doc.id, ...doc.data() },
+                        ]
+                        break
+
+                    case "trending":
+                        trends = [...trends, { id: doc.id, ...doc.data() }]
+                        break
+
+                    default:
+                    // do nothing
+                }
+            })
+
+            dispatch(
+                setMovies({
+                    recommend: recommends,
+                    newDisney: newDisney,
+                    original: originals,
+                    trends: trends,
+                })
+            )
+        })
+    }, [userName])
     return (
         <Container>
             <ImgSlider />
