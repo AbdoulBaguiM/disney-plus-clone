8eb7ea030885bf300b32ec2f93c2e0dfe849a830
Abdelhk <72513211+abdoulbaguimohamadou@users.noreply.github.com>
2022-08-12 22:50:30 +0100

    originals section made dynamic

--- src/components/NewDisney.jsx
+++ src/components/NewDisney.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 import { useSelector } from "react-redux/es/exports"
-import { selectNewDisney, selectRecommmend } from "../features/movie/movieSlice"
+import { selectNewDisney } from "../features/movie/movieSlice"
 
 const NewDisney = (props) => {
     const movies = useSelector(selectNewDisney)
